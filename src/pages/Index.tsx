275e44ea4635ad592f074f2566105dab1c9246f1
Josianne <tjosianne9@gmail.com>
2026-02-24 10:19:40 -0500

    removing header and footer

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,11 @@
-import Header from '@/components/Header';
 import ComparisonTool from '@/components/ComparisonTool';
-import Footer from '@/components/Footer';
 
 const Index = () => {
   return (
     <div className="min-h-screen flex flex-col">
-      <Header />
       <main className="flex-1">
         <ComparisonTool />
       </main>
-      <Footer />
     </div>
   );
 };
